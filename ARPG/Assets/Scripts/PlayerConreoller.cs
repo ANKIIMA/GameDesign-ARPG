100ae616aa1ccfd3622269499527fe2cec928dc7
ANKIIMA <1667791793@qq.com>
2023-06-07 16:18:56 +0800

    Basic locomotion
    Add new Input system for movement, look, run and camera zoom, input done;
    working done the communication between Animation and Scripts;
    Next need to fixed the real move of character controller.

--- ARPG/Assets/Scripts/PlayerConreoller.cs
+++ ARPG/Assets/Scripts/PlayerConreoller.cs
@@ -1,17 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerConreoller : MonoBehaviour
 {
     //Necessary Component
-    private Transform trans;                                                        //Basic Transfrom Methods
+    private Transform trans;                                                            //Basic Transfrom Methods
     private Animator animator;                                                          //Interact with the Animation Controller
     private CharacterController characterController;                                    //Dipose the move and collider of the character
 
     //Intermediate variables
-    private float horizontalSpeed;                                                      //Speed in Horizontal Direction
-    private float verticalSpeed;                                                        //Speed in Vertical Direction
+    private float horizontalTargetSpeed;                                                //Target Speed in Horizontal Direction
+    private float horizontalCurrentSpeed;                                               //Current Speed in Horizontal Direction
+    private float verticalTargetSpeed;                                                  //Speed in Vertical Direction
+    private float verticalCurrentSpeed;                                                 //Current Speed in Vertical Direction
+    private Vector3 playerDirection;                                                    //Target Direction of the Camera
+
+
+
+    [SerializeField, SetProperty("Run Speed")]                                          //Speed when running
+    private float runSpeed = 2;
+    [SerializeField, SetProperty("Walk Speed")]                                         //Speed when walking
+    private float walkSpeed = 1;
+
+    
+
+    //Player Input
+    Vector2 moveInput;
+    bool isRunning;
+
 
     // Start is called before the first frame update
     void Start()
@@ -20,11 +38,57 @@ public class PlayerConreoller : MonoBehaviour
         trans = GetComponent<Transform>();
         animator = GetComponent<Animator>();
         characterController = GetComponent<CharacterController>();
+
+        //Set the cursor to be invisible in the game
+        Cursor.lockState = CursorLockMode.Locked;
+
     }
 
+    #region PlayerInput
+
+    public void GetMoveInput(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    public void GetRunningInput(InputAction.CallbackContext context)
+    {
+        isRunning = context.ReadValueAsButton();
+    }
+
+    #endregion
+    
     // Update is called once per frame
     void Update()
     {
+        calculateTargetDirection();
+        PlayerMoving();
+    }
 
+    private void calculateTargetDirection()
+    {
+        Vector3 cameraForward = Vector3.ProjectOnPlane(Camera.main.transform.forward, Vector3.up).normalized;
+        Vector3 cameraRight = Camera.main.transform.right;
+        playerDirection = moveInput.y * cameraForward + moveInput.x * cameraRight;
+        Debug.Log(playerDirection);
+    }
+
+    //Only deal with the Animation, OnAnimatorMove() include the real movement
+    //Lerp between currentSpeed and taragetSpeed
+    private void PlayerMoving()
+    {
+        verticalTargetSpeed = isRunning ? runSpeed : walkSpeed;
+        animator.SetFloat("Vertical Speed", verticalTargetSpeed * moveInput.magnitude, 0.1f, Time.deltaTime);
+    }
+    
+    private void PlayerRotate()
+    {
+        float rad = Mathf.Atan2(playerDirection.x, playerDirection.z);
+
+    }
+
+    private void OnAnimatorMove()
+    {
+        //characterController.Move(animator.velocity);
     }
 }
